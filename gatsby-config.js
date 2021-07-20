4c51eaec601dfe5274237d464132fc06dc8d50a3
Manu Ramos <mail@manuramos.dev>
2021-07-20 12:05:47 +0200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://manuramos.dev",
+    title: "Manu Ramos",
+  },
+  plugins: ["gatsby-plugin-sass"],
+  plugins: ["gatsby-plugin-typescript"],
+};

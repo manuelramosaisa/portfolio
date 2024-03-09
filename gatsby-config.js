cad3587a750589e38a8422a9ac4be8cf1204f47a
Manu Ramos <mail@manuramos.dev>
2024-03-09 02:42:22 +0100

    leftover gatsby

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: "https://manuramos.dev",
-    title: "Manu Ramos",
-  },
-  plugins: ["gatsby-plugin-sass"],
-  plugins: ["gatsby-plugin-typescript"],
-};
